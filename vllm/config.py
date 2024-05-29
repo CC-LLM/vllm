e97283fb5224098826b81ea5ee5f986669a312a7
daquexian <daquexian566@gmail.com>
2024-05-29 20:07:40 +0800

    move attn_backend after initialization
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- vllm/config.py
+++ vllm/config.py
@@ -11,6 +11,9 @@ from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 from vllm.model_executor.models import ModelRegistry
 from vllm.transformers_utils.config import get_config, get_hf_text_config
 from vllm.utils import get_cpu_memory, is_cpu, is_hip, is_neuron
+from vllm.distributed import (get_tensor_model_parallel_rank,
+                              get_tensor_model_parallel_world_size
+                              )
 
 if TYPE_CHECKING:
     from ray.util.placement_group import PlacementGroup
@@ -18,6 +21,11 @@ if TYPE_CHECKING:
     from vllm.model_executor.model_loader.loader import BaseModelLoader
 
 logger = init_logger(__name__)
+
+def partition_number(total, parts):
+    quotient, remainder = divmod(total, parts)
+    result = [quotient + 1] * remainder + [quotient] * (parts - remainder)
+    return torch.tensor(result)
 
 _GB = 1 << 30
 _EMBEDDING_MODEL_MAX_NUM_BATCHED_TOKENS = 32768
@@ -327,13 +335,18 @@ class ModelConfig:
         # the tensor parallel size. We will replicate the KV heads in the
         # case where the number of KV heads is smaller than the tensor
         # parallel size so each GPU has at least one KV head.
+        tp_rank = get_tensor_model_parallel_rank()
         return max(1,
-                   total_num_kv_heads // parallel_config.tensor_parallel_size)
+                   # total_num_kv_heads // parallel_config.tensor_parallel_size)
+                   partition_number(total_num_kv_heads, parallel_config.tensor_parallel_size)[tp_rank].item())
 
     def get_num_attention_heads(self,
                                 parallel_config: "ParallelConfig") -> int:
-        return self.hf_text_config.num_attention_heads // \
-            parallel_config.tensor_parallel_size
+        tp_rank = get_tensor_model_parallel_rank()
+        # return self.hf_text_config.num_attention_heads // \
+        #             parallel_config.tensor_parallel_size
+        return partition_number(self.hf_text_config.num_attention_heads,
+                                parallel_config.tensor_parallel_size)[tp_rank].item()
 
     def get_num_layers(self, parallel_config: "ParallelConfig") -> int:
         total_num_hidden_layers = self.hf_text_config.num_hidden_layers
