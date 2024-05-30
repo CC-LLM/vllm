db16e3ad632b05703654e8aa2c8b940ae304f2c4
daquexian <daquexian566@gmail.com>
2024-05-30 16:54:44 +0800

    memory eff quant
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -275,6 +275,11 @@ class ColumnParallelLinear(LinearBase):
         assert param_data.shape == loaded_weight.shape
         param_data.copy_(loaded_weight)
 
+        quant_method = getattr(self, "quant_method", None)
+        if quant_method is not None:
+            quant_method.process_weights_after_loading(self)
+            self._processed = True
+
     def forward(self, input_):
         bias = self.bias if not self.skip_bias_add else None
 
@@ -670,6 +675,11 @@ class QKVParallelLinear(ColumnParallelLinear):
         assert param_data.shape == loaded_weight.shape
         param_data.copy_(loaded_weight)
 
+        quant_method = getattr(self, "quant_method", None)
+        if quant_method is not None:
+            quant_method.process_weights_after_loading(self)
+            self._processed = True
+
 
 class RowParallelLinear(LinearBase):
     """Linear layer with row parallelism.
@@ -777,6 +787,11 @@ class RowParallelLinear(LinearBase):
 
         assert param_data.shape == loaded_weight.shape
         param_data.copy_(loaded_weight)
+
+        quant_method = getattr(self, "quant_method", None)
+        if quant_method is not None:
+            quant_method.process_weights_after_loading(self)
+            self._processed = True
 
     def forward(self, input_):
         # Set up backprop all-reduce.
